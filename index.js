efb7e17a679a270dcbfa790e7c5d2e76fd5f2181
taylornorris <taylornorrisdev@gmail.com>
2024-06-29 10:56:29 -0600

    Initialize project

---
+++ index.js
@@ -0,0 +1,8 @@
+import { propertyForSaleArr } from "./properties/propertyForSaleArr.js"
+
+// function getPropertyHtml() {
+
+// }
+
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+// document.getElementById('container').innerHTML = getPropertyHtml()
